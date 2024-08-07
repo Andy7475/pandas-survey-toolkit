ea3912001cd5f6e464206f44354cf3552055edbb
Andrew Laing <Andrew.Laing@PACONSULTING.COM>
2024-08-07 19:36:15 +0100

    fitted sentence transformer

---
+++ pandas-survey-toolkit/src/pandas_survey_toolkit/nlp.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import pandas_flavor as pf
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+@pf.register_dataframe_method
+def fit_sentence_transformer(df, input_column:str, model_name='all-MiniLM-L6-v2', output_column="sentence_embedding"):
+    # Initialize the sentence transformer model
+    df_temp = df.copy()
+    model = SentenceTransformer(model_name)
+    
+    # Create sentence embeddings
+    embeddings = model.encode(df_temp[input_column].tolist())
+    
+    # Convert embeddings to a list of numpy arrays
+    embeddings_list = [np.array(embedding) for embedding in embeddings]
+    
+    # Add the embeddings as a new column in the dataframe
+    df_temp[output_column] = embeddings_list
+    
+    return df_temp
